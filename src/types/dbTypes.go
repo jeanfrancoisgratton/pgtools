46daf15659dd2e1c3c0b2762aaa98c001ce1f12d
jeanfrancoisgratton <jean-francois@famillegratton.net>
2025-09-11 12:41:51 -0400

    types subpackage refactoring

---
+++ src/types/dbTypes.go
@@ -0,0 +1,17 @@
+// pgtools
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original timestamp: 2025/07/04 12:34
+// Original filename: src/environment/dbTypes.go
+
+package types
+
+type DBConfig struct {
+	Host        string `json:"host"`
+	Port        int    `json:"port"`
+	User        string `json:"user"`
+	Password    string `json:"password"`
+	SSLMode     string `json:"sslmode"`
+	SSLCert     string `json:"sslcert,omitempty"`
+	SSLKey      string `json:"sslkey,omitempty"`
+	Description string `json:"comment,omitempty"`
+}
---
+++ src/types/globalFlags.go
@@ -0,0 +1,13 @@
+// pgtools
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original timestamp: 2025/09/11 00:36
+// Original filename: src/types/globalFlags.go
+
+package types
+
+var EnvConfigFile = "defaultEnv.json"
+var DebugMode = false
+var Quiet = false
+var AllDBs = false
+var UserRoles = false
+var LogLevel = "none"
